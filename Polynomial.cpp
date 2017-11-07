e42f44a56d44a61d138761cfe3303d2294e3fbaf
Pradyuman <meetupradyuman@gmail.com>
2017-11-07 16:18:28 +0530

    third commit

--- Polynomial.cpp
+++ Polynomial.cpp
@@ -13,14 +13,26 @@ int main()
     {
         cin>>a[i][0]>>a[i][1]>>a[i][2]>>a[i][3];
     }
-    int q,k;
+    int c,q,k;
     cin>>q;
     int b[q-1],c[n];
-    while(q--)
+    while(q--),min=0;
     {
       cin>>b[q-1];
     }
-
+    for(int j=0;j<(q-1);j++)
+    {
+      for(int k=0;k<n;k++)
+      {
+        c=a[i][0]+a[i][1]*b[j]+a[i][2]*b[j]*b[j]+a[i][3]*b[j]*b[j]*b[j];
+        if(min==0)
+        min=c;
+        else if(c<min)
+        min=c;
+      }
+      cout<<min;
+      min=0;
+    }
   }
     return 0;
 }
