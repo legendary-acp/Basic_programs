84346af4a4cd699aa86964ff4cd6afec72dd38a5
Pradyuman <meetupradyuman@gmail.com>
2017-11-09 22:24:50 +0530

    New File added

---
+++ calender.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+int year, month,year,i;
+int main()
+{
+    printf("Enter the Month and year\n");
+    scanf("%d && %d",&month,&year);
+    if(year%4==0)
+    {
+        if(month==1||3||5||7||8||10||12)
+        {
+            for(i=1;i<=31;i++)
+            {
+                printf("%d\n", i);
+            }
+        }
+        if(month==2)
+        {
+            for(i=1;i<=28;i++)
+            {
+                printf("%d\n",i);
+            }
+        }
+        else
+        {
+            for(i=1;i<=30;i++)
+            {
+                printf("%d\n",i);
+            }
+        }
+        }
+        else
+        {
+            if(month==1||3||5||7||8||10||12)
+        {
+            for(i=1;i<=31;i++)
+            {
+                printf("%d\n", i);
+            }
+        }
+        if(month==2)
+        {
+            for(i=1;i<=27;i++)
+            {
+                printf("%d\n",i);
+            }
+        }
+        else
+        {
+            for(i=1;i<=30;i++)
+            {
+                printf("%d\n",i);
+            }
+        }
+    }
+}
