3fbb88552ff776fcb66e6c89e30d59388719bd55
Pradyuman <meetupradyuman@gmail.com>
2018-10-14 00:17:17 +0530

    dl

--- Rect_to_square.py
+++
@@ -1,6 +0,0 @@
-#soltution to divide a rectangle in minimum number of square plots of equal size
-from math import *
-for i in range(int(input())):
-    (x,y)=[int(p) for p in input().split()]
-    side=gcd(x,y)
-    print(x*y//side**2)
