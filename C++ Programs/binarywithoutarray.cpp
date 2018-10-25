1d98e70c3a959d6d9cb50a8d9ac4cd5ac8fd50e2
Pradyuman <meetupradyuman@gmail.com>
2018-10-25 08:54:54 +0530

    Update binarywithoutarray.cpp

--- C++ Programs/binarywithoutarray.cpp
+++ C++ Programs/binarywithoutarray.cpp
@@ -2,16 +2,14 @@
 #include<math.h>
 int main()
 {
-       int k,m,o=0,a,j,i=0;
-         printf("enter\n");
+       	int k,m,o=0,a,j;
+       	printf("enter\n");
 	scanf("%d",&a);
-		while(a!=0)
-{
-	k=a%2;
-	a/=2;
-	 j+=k*pow(10,i);
-	i++;
-}
-printf("%d\n",j);
-return 0;
+	j=a;
+	while(a!=0){
+		k=a%2;
+		a/=2;
+	}
+	printf("%d\n",j);
+	return 0;
 }
