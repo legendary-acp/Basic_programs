3158a2112ae5ca175b31348bb1e9696d7d87b38a
Pradyuman <meetupradyuman@gmail.com>
2018-10-25 08:48:29 +0530

    Update Tournament.cpp

--- C++ Programs/Tournament.cpp
+++ C++ Programs/Tournament.cpp
@@ -5,7 +5,7 @@ int main()
 {
   long int n;
   cin>>n;
-  long int s=0,a[n],i=1,j;
+  long int s=0,a[n],i=1;
   a[0]=0;
   for(int k=1;k<=n;k++)
   {
@@ -13,7 +13,7 @@ int main()
   }
   while (i<n) 
   {
-    j=i+1;
+    int j=i+1;
     while (j<=n) 
     {
       s=abs(a[i]-a[j])+s;
